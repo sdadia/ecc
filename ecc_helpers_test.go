a6c0a9d713fdc78681cadb46b193ed59d02da93f
sahil <sahil.dadia@hertz.net>
2024-09-16 15:08:59 +0100

    Fixed tests

--- ecc_helpers_test.go
+++ ecc_helpers_test.go
@@ -14,14 +14,3 @@ func TestGenerateRandomBytesArraySize(t *testing.T) {
 		t.Fatalf("The function GenerateRandomBytes generated invalid array size. Expected : %d. Got %d\n", expected_array_size, got_array_size)
 	}
 }
-
-func TestGenerateRandomBytesArrayValuesLessThan255(t *testing.T) {
-	var expected_array_size = uint(16)
-	array := GenerateRandomBytes(expected_array_size)
-	var got_array_size = len(array)
-	for i := range got_array_size {
-		if !((array[i] <= 255) && (array[i] >= 0)) {
-			t.Fatalf("Array values should >= 0 and < 255. Got : %d at index %d\n", array[i], i)
-		}
-	}
-}
